c631722a07e5f4e2c8cc58ff0070d3e3c7caeaf2
Maciej Nowak <mcnowak@wp.pl>
2017-01-23 23:45:12 +0100

    BrowserSync added. Website is using optimized pictures in 'build' folder

--- gruntfile.js
+++ gruntfile.js
@@ -32,6 +32,21 @@ module.exports = function(grunt) {
 					spawn: false,
 				},
 			}
+		},
+
+		browserSync: {
+    		bsFiles: {
+        		src : [
+        			'css/*.css',
+        			'*.html'
+        		]
+    		},
+    		options: {
+    			watchTask: true,
+        		server: {
+            		baseDir: "./"
+	    	   	}
+	    	}
 		}
 
 	});
@@ -40,7 +55,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
 	// Default task(s). 
 
-	grunt.registerTask('default', ["sass", "imagemin", "watch"]); 
+	grunt.registerTask('default', ["sass", "imagemin", "browserSync", "watch"]); 
 };
